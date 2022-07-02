d17137fc8c7ee0282d9a5ec384e4baa3ec0b7f3c
edebezenac <emmanueldebezenac@gmail.com>
2022-07-02 12:38:54 +0200

    starting ModulatedSymmetryNet (a la stylegan)

--- pbc_examples/modules/nn_symmetries.py
+++ pbc_examples/modules/nn_symmetries.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+from pbc_examples.modules.modulated_linear import ModulatedLinear
 from pbc_examples.modules.separation_embedding import Block
 from pbc_examples.modules.separation_param import ParamModule
 from pbc_examples.modules.separation_param_simple_latents import FactorizedMultiplicativeModulation as FMM
@@ -33,11 +34,12 @@ class SymmetryNet(torch.nn.Module, ParamModule):
 
         return u_pred
 
-class ModulatedSymmetryNet(torch.nn.Module, ParamModule):
+
+class FMMSymmetryNet(nn.Module):
     """Add central embedding to the main network"""
 
     def __init__(self, coord_dim, param_dim, out_dim=1, max_rank=10):
-        super(ModulatedSymmetryNet, self).__init__()
+        super(FMMSymmetryNet, self).__init__()
         self.latent_dim = 128
         self.max_rank = max_rank
         num_blocks = 4
@@ -70,34 +72,45 @@ class ModulatedSymmetryNet(torch.nn.Module, ParamModule):
         return h
 
 
-def param_linear(input, weight, bias=None):
-    '''
-    The difference with the other ParamLinear linear in the code is the fact that
-    there are no weights that are saved in memory that are not used anyway
-    '''
-    # bias = params.get('bias', None)
-    # weight = params['weight']
-    output = input.matmul(weight.permute(*[i for i in range(len(weight.shape) - 2)], -1, -2))
-    if bias is not None:
-        output += bias.unsqueeze(-2)
-    return output
+class ModulatedSymmetryNet(nn.Module):
+    """Add central embedding to the main network"""
 
+    def __init__(self, coord_dim, z_dim, hidden_dim=128, out_dim=1, num_blocks=4, ):
+        super(ModulatedSymmetryNet, self).__init__()
 
-class ModulatedLinear(nn.Linear):
-    """Fully connected version of stylegan2's modulated conv"""
-    def __init__(self, in_features, out_features, bias=True):
-        super(ModulatedLinear, self).__init__(in_features, out_features, bias=bias)
-        self.eps = 1e-8
+        mlins = []
+        affines = []
+        for i in range(num_blocks):
+            if i == 0:
+                in_dim = coord_dim
+            else:
+                in_dim = hidden_dim
+            if i == num_blocks - 1:
+                out_dim = out_dim
+            else:
+                out_dim = hidden_dim
+            # in_features, out_features, in_mod_features, rank, bias = True
+            mlin = ModulatedLinear(in_dim, out_dim, bias=True)
+            affine = nn.Linear(in_dim, hidden_dim, bias=True)
+            mlins.append(mlin)
+            affines.append(affine)
+        self.mlins = nn.ModuleList(mlins)
+        self.affines = nn.ModuleList(affines)
+        self.style_net = DNN([z_dim, hidden_dim, hidden_dim, hidden_dim], 'relu')
 
-    def forward(self, input, style):
+    def forward(self, coords, z, ):
         """
-        style: (B, I), weight: (O, I), input: (B, I), bias: (B, O)
-        output: (B, O)
+        coords: (B, N, coord_dim)
+        z: (B, N, z_dim)
         """
-        assert self.weight.shape[1] == style.shape[1]
-        # mod_weight: (B, O, I)
-        mod_weight = self.weight.unsqueeze(0) * style.unsqueeze(1)
-        norm_weight = mod_weight.pow(2).sum(-1, keepdim=True)
-        demod_weight = mod_weight / torch.sqrt(norm_weight + self.eps)
-        output = param_linear(input, demod_weight, self.bias)
-        return output
+        w = self.style_net(z)
+        h = coords
+        for i in range(len(self.mlins) - 1):
+            a = self.affines[i](w)
+            h = self.mlins[i](h, a)
+            h = torch.relu(h)
+        a = self.affines[-1](w)
+        h = self.mlins[-1](h, a)
+        return h
+
+
--- pbc_examples/test_mod.py
+++ pbc_examples/test_mod.py
@@ -1,6 +1,7 @@
 import torch
 
-from pbc_examples.modules.nn_symmetries import ModulatedLinear
+from pbc_examples.modules.nn_symmetries import ModulatedLinear, ModulatedSymmetryNet
+
 # from pbc_examples.modules.separation_param_simple_latents import FactorizedMultiplicativeModulation as FMM
 
 if __name__ == '__main__':
@@ -9,8 +10,17 @@ if __name__ == '__main__':
     # input = torch.randn(5, 2)
     # mod = torch.randn(5, 3)
     # print(lin(input, mod).shape)
+    #
+    # mlin = ModulatedLinear(2, 3, bias=True)
+    # input = torch.ones(1, 2)
+    # style = torch.ones(1, 2) * 2
+    # print(mlin(input, style))
 
-    mlin = ModulatedLinear(2, 3, bias=True)
-    input = torch.ones(5, 2)
-    style = torch.ones(5, 2) * 2
-    print(mlin(input, style).shape)
+    coord_dim = 2
+    z_dim = 3
+    B = 5
+    net = ModulatedSymmetryNet(coord_dim=coord_dim, z_dim=z_dim, out_dim=1)
+    z = torch.randn(B, 7, z_dim)
+    coords = torch.randn(B, 7, coord_dim)
+    out = net(coords, z)
+    print(out.shape)
